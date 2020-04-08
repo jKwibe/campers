22a0bf26a8cbdd98580991677c81ffc5d23ed5f9
Kwibe Merci <kwibemerci12@gmail.com>
2020-04-07 18:21:33 -0600

    errors clean up

--- middleware/error.js
+++ middleware/error.js
@@ -1,22 +1,30 @@
-
+// const colors = require('colors');
 const ErrorResponse = require('../helpers/error');
 
 const errorHandler = (err, req, res, next)=>{
   let error = {...err};
 
   error.message = err.message;
-  console.log(err.stack.red);
+  console.log(err);
   console.log(err.code)
     // Error with an impropper // ID
     if (err.name === 'CastError'){
       const message = `Resource not found with an improper id ${err.value}`;
-      error = new ErrorResponse(message, 400);
+      error = new ErrorResponse(message, 404);
     }
     // Mongo duplication error handler
     if(err.code === 11000){
       const message = `Resource already exist`
       error = new ErrorResponse(message, 400);
     }
+
+    // For database validation errors
+    if(err.name === 'ValidationError'){
+      const message = Object.values(err.errors).map(val => val.message);
+      error = new ErrorResponse(message, 400);
+    }
+
+
   res.status(error.statusCode || 500).json({
     sucess: false,
     err: error.message || 'Server error'
