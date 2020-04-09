a258e3886ce664a94505ba7a78f53a0455c04d41
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-09 13:23:24 -0600

    controller logic to fetch data for a particular bootcamp

--- controllers/courses.js
+++ controllers/courses.js
@@ -6,17 +6,30 @@ const Course = require('../models/Courses');
 
 // @desc    Fetch all courses
 // @desc    GET  /api/v1/courses/
+// @desc    GET /api/v1/bootcamps/:bootcampid/courses
 // @access  Public
 
-// @desc    GET /api/v1/:bootcampid/courses
-// @access  Private
 
 exports.getCourses =  asyncHandler(async (req, res, next) => {
 
-// fetch all bootcamps
-const courses = await Course.find({})
+let query;
+
+// console.log(req.params.bootcampid)
+  // Check for the parameter if it has a bootcamp id and sort for courses with that id
+
+if(req.params.bootcampid){
+
+  query= Course.find({bootcamp: req.params.bootcampid});
+
+}else{
+  query = Course.find({});
+}
+
+// fetch all courses even with a single bootcamp or display all courses
+const courses = await query;
   res.status(200).json({
     success: true,
+    count: courses.length,
     data: courses
   })
 })
