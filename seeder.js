aadc94f1463c0ea61ac222f093d36bbba382d546
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-09 00:08:20 -0600

    Seeder update to fetch the course data

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@
 
  //loadmodals
 const Bootcamp = require('./models/Bootcamps');
+const Course = require('./models/Courses');
 
 //connect to // DEBUG:
 mongoose.connect(process.env.MONGO_DB_URI, {
@@ -19,11 +20,13 @@ mongoose.connect(process.env.MONGO_DB_URI, {
 
 // read JSON file
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
 
 // import in to db
 const importData = async ()=>{
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("data imported".green.inverse);
     process.exit();
   } catch (err) {
@@ -36,6 +39,7 @@ const importData = async ()=>{
 const deleteData = async ()=>{
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("data destroyed".red.inverse);
     process.exit();
   } catch (err) {
