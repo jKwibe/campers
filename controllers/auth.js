36cbd6e5315211f6790af2314b1bee12edd77dbd
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-11 17:04:28 -0600

    post user details for registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,9 +6,21 @@ const User = require('../models/Users');
 const ErrorResponse = require('../helpers/error');
 const asyncHandler = require('../middleware/async');
 
-
+// @desc    Register User
+// @desc    POST  /api/v1/auth/register
+// @access  Public
 
 exports.registerUser = asyncHandler(async (req, res, next)=>{
+    const {name, email, password, role} = req.body;
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role
+    });
+
 
     res.status(201).json({
       success: true
